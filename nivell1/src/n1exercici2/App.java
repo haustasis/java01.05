9a66804cc389d9fc0138728bcb5ddde2a46408d8
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-02-13 20:31:30 +0100

    ex2 D F modified

--- nivell1/src/n1exercici2/App.java
+++ nivell1/src/n1exercici2/App.java
@@ -5,9 +5,33 @@ amb el contingut de tots els seus nivells (recursivamente) de manera que s'impri
 en ordre alfabètic dins de cada nivell, indicant a més si és un directori (D) o un fitxer (F),
 i la seva última data de modificació.
  */
+import java.io.File;
+import java.util.Arrays;
+
 public class App {
 
     public static void main(String[] args) {
+
+        File directorio = new File("nivell1");
+
+        String[] directorioContenido = directorio.list();
+
+        Arrays.sort(directorioContenido);
+
+        for(int i = 0; i < directorioContenido.length; i++) {
+            System.out.println(directorioContenido[i]);
+
+            File directorioArchivos = new File(directorio.getAbsolutePath(), directorioContenido[i]);
+
+            if (directorioArchivos.isDirectory()) {
+                String[] subDirectorio = directorioArchivos.list();
+                Arrays.sort(subDirectorio);
+
+                for(int j = 0; j < subDirectorio.length; j++) {
+                    System.out.println(subDirectorio[j]);
+                }
+            }
+        }
 
     }
 }
