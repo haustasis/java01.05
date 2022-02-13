740ebe5af5d6fda27f8848a10eda61c327ae628b
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-02-13 21:54:30 +0100

    ex4 printStream setout console resultat0

--- nivell1/src/n1exercici4/App.java
+++ nivell1/src/n1exercici4/App.java
@@ -1,10 +1,54 @@
 package n1exercici4;
-/*
-Exercici 4. Afegeixi la funcionalitat de llegir qualsevol fitxer txt i mostrar per consola el seu contingut.
- */
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Date;
+
 public class App {
 
     public static void main(String[] args) {
 
+        // LLEGIR DIRECTORIS I GUARDAR EN UN ARCHIU
+        PrintStream console = System.out;
+        try {
+            File directorio = new File("nivell1");
+            PrintStream fileOut = new PrintStream("resultat.txt");
+            System.setOut(fileOut);
+            verDirectorioContenido(directorio);
+        } catch(FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+
+        // LLEGIR FITXER TXT I MOSTRAR PER CONSOLA
+        try {
+        File file = new File("resultat0.txt");
+        BufferedReader br = null;
+        br = new BufferedReader(new FileReader(file));
+        String st;
+        System.setOut(console);
+            while ((st = br.readLine()) != null) {
+                System.out.println(st);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void verDirectorioContenido(File directorio) {
+        try {
+            File[] archivos = directorio.listFiles();
+            Arrays.sort(archivos);
+            for (File archivo : archivos) {
+                Date lastMod = new Date(archivo.lastModified());
+                if (archivo.isDirectory()) {
+                    System.out.println("(D) " + archivo.getCanonicalPath() + " " + lastMod);
+                    verDirectorioContenido(archivo);
+                } else {
+                    System.out.println("(F) " + archivo.getCanonicalPath() + " " + lastMod);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
